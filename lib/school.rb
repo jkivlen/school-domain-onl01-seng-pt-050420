7e40aea31fd929e535f699622f0ceddd2ec66cef
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-28 23:48:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]
+    @roster[grade] = []
+  end
+    @roster[grade].push(name)
+  end
+  
+end
+
